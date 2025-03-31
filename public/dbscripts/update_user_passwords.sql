8331a8007524cc34ab44b592fb7e1665faa8c1af
Roope myllymäki <s2301215@edu.bc.fi>
2025-03-31 09:55:05 +0300

    Korjattu tehtava 5

--- public/dbscripts/update_user_passwords.sql
+++ public/dbscripts/update_user_passwords.sql
@@ -1,6 +1,12 @@
 ALTER TABLE system_user 
 ADD COLUMN ´password´ VARCHAR(100) NULL;
 
-UPDATE system_user 
-SET password = '<hashed_password>' 
-WHERE id = <user_id>;
+update system_user
+SET password = '$2b$10$YFgdsCJC0d8y1kPIX.bOEOBicbxO/GTJx.P3hlAeq6m.0nyeQREtu'
+WHERE id = 14;
+
+ALTER TABLE system_user 
+
+update system_user
+SET password = '$2b$10$i/WcriPki6i.3CS9MSnh6.UwEc/EjrLAHDkKhU4dXAkwDWvZD.dvW'
+WHERE id = 15;
